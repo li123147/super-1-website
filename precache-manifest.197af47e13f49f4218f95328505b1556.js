00c5cc6ba5f5b520a942e27132e422e4212c967e
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-07-06 18:13:04 +0800

    init

---
+++ precache-manifest.197af47e13f49f4218f95328505b1556.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aefb887d9485a66348a6f7306d929315",
+    "url": "/super-1-website/index.html"
+  },
+  {
+    "revision": "5bdd2437443fff41fadd",
+    "url": "/super-1-website/static/css/main.339b420b.chunk.css"
+  },
+  {
+    "revision": "045831fa7fe87dcbc019",
+    "url": "/super-1-website/static/js/2.4405536c.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/super-1-website/static/js/2.4405536c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bdd2437443fff41fadd",
+    "url": "/super-1-website/static/js/main.09c3d69f.chunk.js"
+  },
+  {
+    "revision": "d520e8c246e1603e3bee",
+    "url": "/super-1-website/static/js/runtime-main.0f90bfb8.js"
+  }
+]);
